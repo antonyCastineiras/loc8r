da60d67bf8774866e1826b14d3dc6dbf14aca231
antonyCastieniras <antonycastineiras@gmail.com>
2017-11-28 21:10:41 +0000

    updated

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://localhost/loc8r';
 
-if( process.env.NODE_ENV ==+ 'production' ) {
+if( process.env.NODE_ENV === 'production' ) {
 	dbURI = process.env.MONGODB_URI
 }
 
