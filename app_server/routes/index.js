adc2886734c35bf1038d389ca693a6a6012996a4
antonyCastieniras <antonycastineiras@gmail.com>
2017-11-28 13:20:06 +0000

    added views to prototype

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlOthers = require('../controllers/others');
+var ctrlLocations = require('../controllers/locations')
+
+/* Location Pages */
+router.get('/', ctrlLocations.homelist)
+router.get('/location', ctrlLocations.locationInfo)
+router.get('/location/review/new', ctrlLocations.addReview)
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
