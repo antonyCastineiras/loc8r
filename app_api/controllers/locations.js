21a76b725ff1a26847211a661d41f28cd8da9c80
antony castineiras <antonycastineiras@gmail.com>
2017-12-01 17:23:43 +0000

    completed chapter 6

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -23,16 +23,26 @@ var sendJsonResponse = function(res, status, content) {
 	res.json(content);
 }
 
-var formatGeoResponse = function(doc) {
+var locationFromParams = function(reqBody) {
 	return {
-		distance: theEarth.getDistanceFromRads(doc.dis),
-		name: doc.obj.name,
-		address: doc.obj.address,	
-		rating: doc.obj.rating,
-		facilities: doc.object.facilities,
-		_id: doc.obj._id
-	}
+		name: reqBody.name,
+		address: reqBody.address,
+		facilities: reqBody.facilities,
+		coords: [parseFloat(reqBody.lng), parseFloat(reqBody.lat)],
+		openingTimes: [{
+			days: reqBody.days1,
+			opening: reqBody.opening1,
+			closing: reqBody.closing1,
+			closed: reqBody.closed1
+		}, {
+			days: reqBody.days2,
+			opening: reqBody.opening2,
+			closing: reqBody.closing2,
+			closed: reqBody.closed2
+		}]
+	};
 }
+
 // INDEX
 module.exports.locationsListByDistance = function(req, res) {
 	var lng = parseFloat(req.query.lng);
@@ -44,22 +54,37 @@ module.exports.locationsListByDistance = function(req, res) {
 	};
 	var geoOptions = {
 		spherical: true,
-		maxDistance: 200000,
+		maxDistance: 2000000,
 		limit: 10
 	};
 
+	if ( !lng || !lat ) {
+		sendJsonResponse(res, 404, { "message": "lattiude and longitude query parameters are requried"});
+		return;
+	}  
+
 	Loc.geoNear(point, geoOptions, function(err, results, stats) {
 		var locations = [];
-		results.forEach( function(doc) {
-			console.log(doc)
-			locations.push(formatGeoResponse(doc));
-		});
-		sendJsonResponse(res, 200, locations)
+		if(err) {
+			sendJsonResponse(res, 404, err);
+		} else {
+			for (result in results) {
+				locations.push(results[result])
+			}
+			sendJsonResponse(res, 200, locations);
+		}
 	});
 }
 // CREATE
 module.exports.locationsCreate = function(req, res) {
-	sendJsonResponse(res, 200, { "status": "success" })
+	console.log(req.body)
+	Loc.create( locationFromParams(req.body), function(err, location) {
+		if(err) {
+			sendJsonResponse(res, 400, err)
+		} else {
+			sendJsonResponse(res, 201, location)
+		}
+	})
 }
 // READ
 module.exports.locationsReadOne = function(req, res) {
@@ -80,9 +105,58 @@ module.exports.locationsReadOne = function(req, res) {
 }
 // UPDATE
 module.exports.locationsUpdateOne = function(req, res) {
-	sendJsonResponse(res, 200, { "status": "success" })
+	console.log(req.body)
+	if(!req.params.locationid) {
+		sendJsonResponse(res, 404, { "message" : "Not found, locationid is required" });
+		return;
+	}
+	Loc.findById(req.params.locationid).select('-reviews -rating').exec(function(err, location) {
+		if(!location) {
+			sendJsonResponse(res, 404, { "message" : "locationid not found" });
+			return;
+		} else if(err) {
+			sendJsonResponse(res, 400, err);
+			return;
+		}
+		location.name = req.body.name;
+		location.address = req.body.address;
+		location.facilities = req.body.facilities.split(", ");
+		location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+		location.openingTimes = [{
+			days: req.body.days1,
+			opening: req.body.opening1,
+			closing: req.body.closing1,
+			closed: req.body.closed1
+		},{
+			days: req.body.days2,
+			opening: req.body.opening2,
+			closing: req.body.closing2,
+			closed: req.body.closed2
+		}];
+		location.save(function(err, location) {
+			if(err) {
+				sendJsonResponse(res, 404, err);
+			} else {
+				sendJsonResponse(res, 200, location);
+			}
+		})
+	})
 }
 // DELETE
 module.exports.locationsDeleteOne = function(req, res) {
-	sendJsonResponse(res, 200, { "status": "success" })
+	var locationid = req.params.locationid;
+	if(locationid) {
+		Loc.findByIdAndRemove(locationid).exec(function(err, location) {
+			if(err) {
+				sendJsonResponse(res, 404, err);
+				return;
+			} else {
+				sendJsonResponse(res, 204, null);
+			}
+		});
+	} else {
+		sendJsonResponse(res, 404, { "message" : "No locationid"});
+	}
 }
+
+
--- app_api/models/locations.js
+++ app_api/models/locations.js
@@ -11,7 +11,7 @@ var openingTimeSchema = new mongoose.Schema({
 	days: { type: String, required: true },
 	opening: String,
 	closing: String,
-	closed: { type: Boolean, required: true }
+	closed: { type: Boolean }
 });
 
 var locationSchema = new mongoose.Schema({
@@ -19,7 +19,7 @@ var locationSchema = new mongoose.Schema({
 	address: String,
 	rating: { type: Number, "default": 0, min: 0, max: 5 },
 	facilities: [String],
-	coords: { type: [Number], index: '2dsphere' },
+	coords: { type: [Number], index: '2dsphere', required: true },
 	openingTimes: [openingTimeSchema],
 	reviews: [reviewSchema]
 });
